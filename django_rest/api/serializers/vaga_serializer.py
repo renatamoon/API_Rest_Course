6a0aa20953aed2ab49de0a277eb47e856a6d7fd9
renata monteiro <r.cardoso.monteiro1@gmail.com>
2021-11-30 10:47:10 -0300

    final commit

--- django_rest/api/serializers/vaga_serializer.py
+++ django_rest/api/serializers/vaga_serializer.py
@@ -3,11 +3,25 @@
 
 from rest_framework import serializers
 from ..models import Vaga
+from rest_framework.reverse import reverse
 
 class VagaSerializer(serializers.ModelSerializer):
+    links = serializers.SerializerMethodField()
     class Meta:
         model = Vaga #model de base para verificação se os dados
         #estão validos - no caso somente vamos validar o unico campo que é
         #o nome da Tecnologia - validacao de dados que entrarão no banco de dados
         fields = ('id', 'titulo','descricao', 'salario', 'local', 'quantidade',
-        'contato', 'tipo_contratacao', 'tecnologias')
+        'contato', 'tipo_contratacao', 'tecnologias', 'links', )
+    
+    
+    def get_links(self, obj):
+        #obter a requisicao que veio da nossa app
+        request = self.context['request']
+        #retorna os links para manipular determinada vaga
+        return {
+             #criando uma rota para o vaga-detalhes
+            'self': reverse('vaga-detalhes', kwargs={'id': obj.pk}, request=request),           
+            'delete': reverse('vaga-detalhes', kwargs={'id': obj.pk}, request=request),
+            'update': reverse('vaga-detalhes', kwargs={'id': obj.pk}, request=request)
+        }
--- django_rest/api/views/vagas_views.py
+++ django_rest/api/views/vagas_views.py
@@ -17,7 +17,7 @@ class VagaList(APIView):
         paginacao = LimitOffsetPagination()
         vagas = vagas_service.listar_vagas()
         resultado = paginacao.paginate_queryset(vagas, request)        
-        serializer = vaga_serializer.VagaSerializer(resultado, many=True)
+        serializer = vaga_serializer.VagaSerializer(resultado, context={'request':request}, many=True)
         return paginacao.get_paginated_response(serializer.data)
         #return Response(Serializer.data, status=status.HTTP_200_OK)
 
